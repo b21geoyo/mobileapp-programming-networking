39340acbd0e2f205bd1d4881c37001363205f9ac
b21geoyo <b21geoyo@student.his.se>
2022-08-12 21:27:18 +0200

    Created Mountain class

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,14 @@
+package com.example.networking;
+
+public class Mountain {
+
+    private String name;
+
+    public Mountain(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
